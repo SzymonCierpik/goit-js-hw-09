3b9863302497454013d6dc5fe600c03d66844d17
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-11 22:45:05 +0100

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,38 +1,73 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-flatpickr(
-  '#datetime-picker',
-  (options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  })
-);
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+const startButton = document.querySelector('[data-start]');
+startButton.setAttribute(`disabled`, `disabled`);
+
+let targetDate;
+let options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startButton.removeAttribute('disabled');
+      targetDate = new Date(selectedDates[0]).getTime();
+      const newTime = convertMs(targetDate - new Date().getTime());
+      days.textContent = formatNumber(newTime.days);
+      hours.textContent = formatNumber(newTime.hours);
+      minutes.textContent = formatNumber(newTime.minutes);
+      seconds.textContent = formatNumber(newTime.seconds);
+    }
+  },
+};
+
+let intervalId;
+startButton.addEventListener(`click`, function () {
+  intervalId = setInterval(function () {
+    const newTime = convertMs(targetDate - new Date().getTime());
+    days.textContent = formatNumber(newTime.days);
+    hours.textContent = formatNumber(newTime.hours);
+    minutes.textContent = formatNumber(newTime.minutes);
+    seconds.textContent = formatNumber(newTime.seconds);
+
+    if (targetDate - new Date().getTime() <= 0) {
+      clearInterval(intervalId);
+      days.textContent = '00';
+      hours.textContent = '00';
+      minutes.textContent = '00';
+      seconds.textContent = '00';
+    }
+  }, 1000);
+});
+
+function formatNumber(num) {
+  return num.toString().padStart(2, '0');
+}
+
+flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(1);
+// console.log(2);
+// console.log(3);
